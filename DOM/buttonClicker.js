84b130ddc180a95aa9904bd8649927d379f2c6eb
Craig Burke <cragardev@gmail.com>
2022-02-26 01:36:54 -0600

    add onclicks and js

--- DOM/buttonClicker.js
+++ DOM/buttonClicker.js
@@ -1,31 +1,15 @@
 console.log("Button Checker JS");
 
-function example(element) {
-  console.log("element clicked", element);
-  console.log(element.attributes);
-  console.log(element.innerText);
-  console.log(element.innerHTML);
-  console.log(element.outerHTML);
-  console.log(element.lastChild);
+function logIn(element) {
+  element.innerText = "Logout";
 }
 
-function turnOff(element) {
-  if (element.innerText == "On") {
-    element.innerText = "Off";
-    element.style.backgroundColor = "Red";
-    element.style.color = "White";
-  } else {
-    element.innerText = "On";
-    element.style.backgroundColor = "Blue";
-  }
-}
-
-function hide(element) {
+function removeMe(element) {
   element.remove();
-  console.log(element.alt);
-  console.dir(`You have removed the ${element.alt}!!!`);
 }
 
-function grow(element) {
-  element.width *= 2;
+function likeBtnClicked(element) {
+  alert("Ninja was liked");
 }
+
+console.log();
